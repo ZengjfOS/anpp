306658f8b4acf58b1e0a37d66b0e1732ff17b9b0
ZengjfOS <zengjf42@163.com>
2023-11-18 13:38:54 +0800

    support Windows cmd

--- src/anpp/ShellCMD.py
+++ src/anpp/ShellCMD.py
@@ -7,15 +7,14 @@ def Shell(cmd):
     if sys.platform.startswith('linux'):
         out = subprocess.Popen(["bash", "-c", cmd], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     elif sys.platform.startswith('win32'):
-        pass
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     elif sys.platform.startswith('darwin'):
         pass
 
-    out.wait()
-
     if out != None:
+        out.wait()
+
         data = out.stdout.read()
-        print(out.returncode)
         if data != None:
             return {"status": out.returncode, "output": data.decode('utf-8').strip()}
         else:
